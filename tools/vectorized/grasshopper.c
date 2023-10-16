51219f4a4a7eb4b6baf483e49ec614eb3e81ffff
vb-sc <vladislav.belov@syntacore.com>
2023-10-16 22:13:27 +0300

    [RVV]: made a copy of simple implementation with minor vectorizing

--- tools/vectorized/grasshopper.c
+++ tools/vectorized/grasshopper.c
@@ -1,5 +1,7 @@
 #include "grasshopper/grasshopper.h"
 
+#include <riscv_vector.h>
+
 #include <string.h>
 #include <stdio.h>
 
@@ -23,7 +25,7 @@ Block Coefs [NUM_ROUNDS / 2 - 1] [BIT_IN_BYTES];
 unsigned char Muls [256] [16];
 unsigned char LS_tbl [16] [256] [16];
 
-void grasshoper_encrypt(Block *block, Key *key)
+void grasshopper_encrypt(Block *block, Key *key)
 {
   //TODO: remove it with autogenerating for tables
   static int generate = 1;
@@ -68,8 +70,15 @@ static void generate_keys (Keys_Array keys, Key *key)
 static void
 apply_x(Block *block, const Block *key)
 {
-  for (int i = 0; i < BLOCK_SIZE; ++i)
-    block->data [i] = block->data [i] ^ key->data [i];
+  // XOR two 16-bytes arrays block->data and key->data.
+  // The result puts into block->data
+  unsigned vl = __riscv_vsetvl_e8m1(BLOCK_SIZE);
+  vuint8m1_t v1 = __riscv_vle8_v_u8m1(block->data, vl);
+  vuint8m1_t v2 = __riscv_vle8_v_u8m1(key->data, vl);
+  vuint8m1_t vd;
+
+  vd = __riscv_vxor_vv_u8m1(v1, v2, vl);
+  __riscv_vse8_v_u8m1(block->data, vd, vl);
 }
 
 static void apply_l (Block *block)
@@ -105,9 +114,11 @@ static void apply_ls (Block *block)
   for (int i = 0; i < BLOCK_SIZE; ++i)
     for (int j = 0; j < BLOCK_SIZE; ++j)
       tmp [j] ^= LS_tbl [i] [block->data [i]] [j];
-
-  for (int i = 0; i < BLOCK_SIZE; ++i)
-    block->data [i] = tmp [i];
+  
+  // memcpy pattern (but better for RISCV!)
+  unsigned vl = __riscv_vsetvl_e8m1(BLOCK_SIZE);
+  vuint8m1_t v_tmp = __riscv_vle8_v_u8m1(tmp, vl);
+  __riscv_vse8_v_u8m1(block->data, v_tmp, vl);
 }
 
 static void apply_xsl (Block *block, const Block *key)
--- tools/vectorized/main.c
+++ tools/vectorized/main.c
@@ -1,1 +1,46 @@
-int main () { return 0; }
+#include <stdlib.h>
+#include <stdio.h>
+#include "config.h"
+#include "grasshopper/grasshopper.h"
+#include "file_common.h"
+
+int main (int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    fprintf (stderr, "Usage: ./grasshopper test_file\n");
+    return _EXIT_CODE;
+  }
+
+  FILE *input;
+  if (!(input = fopen (argv [1], "r")))
+  {
+    fprintf (stderr, "Can't open %s test\n", argv [1]);
+    return _EXIT_CODE;
+  }
+
+  size_t file_size = get_file_size (input);
+  Block *buffer = (Block *)allocate_with_padding
+		    (file_size, BLOCK_SIZE, &file_size);
+  fread (buffer, sizeof (char), file_size, input);
+
+  Key key = {{0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x00, 0x11, 0x22,
+	      0x33, 0x44, 0x55, 0x66, 0x77},
+	     {0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10, 0x01, 0x23, 0x45,
+	      0x67, 0x89, 0xab, 0xcd, 0xef}};
+
+  for (size_t block_ind = 0; block_ind < file_size / BLOCK_SIZE; ++block_ind)
+  {
+    grasshopper_encrypt (buffer + block_ind, &key);
+#if 1
+    fprintf (stdout, "Running %ld/%ld block: ", block_ind, file_size / BLOCK_SIZE);
+    for (int i = 0; i < BLOCK_SIZE; ++i)
+      fprintf (stdout, "%c", (buffer + block_ind)->data [i]);
+    fprintf (stdout, "\n");
+#endif
+  }
+
+  fclose (input);
+  free (buffer);
+  return 0;
+}
