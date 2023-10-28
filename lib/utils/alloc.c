e789c42483c1597f5405e3bd62a804840ab4e538
vb-sc <vladislav.belov@syntacore.com>
2023-10-28 18:11:16 +0300

    [clang-format]: added llvm-style clang-format config

--- lib/utils/alloc.c
+++ lib/utils/alloc.c
@@ -2,8 +2,7 @@
 
 #include "config.h"
 
-void *allocate_with_padding (size_t size, size_t padding, size_t *res_size)
-{
-    *res_size = size + padding - (size % padding);
-    return calloc (*res_size, sizeof (char));
+void *allocate_with_padding(size_t size, size_t padding, size_t *res_size) {
+  *res_size = size + padding - (size % padding);
+  return calloc(*res_size, sizeof(char));
 }
--- lib/utils/file_common.c
+++ lib/utils/file_common.c
@@ -1,13 +1,10 @@
 #include "file_common.h"
 #include "config.h"
 
-size_t
-get_file_size (FILE *f)
-{
-  fseek (f, 0, SEEK_END);
-  size_t ret = ftell (f);
-  fseek (f, 0, SEEK_SET);
+size_t get_file_size(FILE *f) {
+  fseek(f, 0, SEEK_END);
+  size_t ret = ftell(f);
+  fseek(f, 0, SEEK_SET);
 
   return ret;
 }
-
