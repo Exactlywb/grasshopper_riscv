ab25019351804acd858d46f618f4234c63ccb0f7
vb-sc <vladislav.belov@syntacore.com>
2023-10-30 12:54:39 +0300

    [RVV]: vectorized apply_l and apply_inv_l partially

--- tools/simple/simple.c
+++ tools/simple/simple.c
@@ -32,3 +32,16 @@ void apply_ls(Block *block) {
 
   memcpy(block->data, tmp, BLOCK_SIZE);
 }
+
+void apply_inv_l(Block *block) {
+  for (int i = 0; i < BLOCK_SIZE; ++i) {
+    unsigned char elem = block->data[0];
+    memmove(block->data, block->data + 1, BLOCK_SIZE - 1);
+    block->data[BLOCK_SIZE - 1] = elem;
+
+    unsigned char tmp = 0;
+    for (int j = 0; j < BLOCK_SIZE; ++j)
+      tmp ^= Muls[block->data[j]][j];
+    block->data[BLOCK_SIZE - 1] = tmp;
+  }
+}
